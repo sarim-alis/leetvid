166db667b871b0ef8a7180db42d62a840dab62bf
sarim-alis <sarimslayerali786@gmail.com>
2026-02-08 00:29:04 +0500

    feat: changes convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-     <h1>Home 🏡</h1>
+    <div className="m-10">
+     <SignedOut>
+       <SignInButton />
+     </SignedOut>
+
+     <SignedIn>
+       <UserButton />
+     </SignedIn>
     </div>
   );
 }
