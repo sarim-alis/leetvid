d07af443f1e06c096d7543e32fa3ed87c3314123
sarim-ais <sarimslayerali786@gmail.com>
2026-03-01 14:51:36 +0500

    feat: get calls by id

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,4 +1,32 @@
+"use client";
+
+import LoaderUI from "@/components/LoaderUI"
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+
 function MeetingPage() {
-  return <div>MeetingPage</div>;  
+  const { id } = useParams();
+  const { isLoaded } = useUser();
+  const { call, isCallLoading } = useGetCallById(id);
+
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <LoaderUI />;
+
+  return  (
+  <StreamCall call={call}>
+    <StreamTheme>
+     {!isSetupComplete ? (
+      <MeetingSetup  onSetupComplete={() => setIsSetupComplete(true)} />
+     ) : (
+      <MeetingRoom />
+     )}
+     </StreamTheme>
+  </StreamCall>
+  );
 }
 export default MeetingPage;
---
+++ src/components/MeetingSetup.tsx
@@ -0,0 +1,4 @@
+function MeetingSetup() {
+  return (<div>MeetingSetup</div>)
+}
+export default MeetingSetup;
