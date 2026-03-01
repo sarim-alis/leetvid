da8f8d6997eae7c7df229cbd77922c24a871e04a
sarim-ais <sarimslayerali786@gmail.com>
2026-03-01 09:47:58 +0500

    feat; modal

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   const router = useRouter();
 
   const { isInterviewer, isCandidate, isLoading } = useUserRole();
-  const interviews = useQuery(api.interviews.getMyInterviews);
+  // const interviews = useQuery(api.interviews.getMyInterviews);
   const [showModal, setShowModal] = useState(false);
   const [modalType, setModalType] = useState<"start" | "join">();
 
