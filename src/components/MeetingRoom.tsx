5179e27c896257d27cce22eb44c7e1a657b59641
sarim-alis <sarimslayerali786@gmail.com>
2026-03-09 07:26:40 +0500

    feat: meeting room

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,4 +1,4 @@
-import { CallingState, useCallStateHooks } from "@stream-io/video-react-sdk";
+import { CallingState, CallParticipantsList, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from "@stream-io/video-react-sdk";
 import { LoaderIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -7,12 +7,10 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "./ui/resiz
 function MeetingRoom() {
     const router = useRouter();
     const [layout, setLayout] = useState<"grid" |"speaker">('speaker');
-    // const { showParticipants, setShowParticipants } = useState(false);
-
+    const { showParticipants, setShowParticipants } = useState(false);
     const { useCallCallingState } = useCallStateHooks();
 
     const callingState = useCallCallingState();
-
     if (callingState !== CallingState.JOINED) {
         return (
             <div className="h-screen flex items-center justify-center">
@@ -22,11 +20,20 @@ function MeetingRoom() {
     }
 
     return (
-    <div className="h-[calc(100vh-4rem)]">
+    <div className="h-[calc(100vh-4rem-1px)]">
         <ResizablePanelGroup className="h-full flex flex-row" data-panel-group-direction="horizontal">
-            <ResizablePanel defaultSize={35} minSize={25}>
-                <div className="h-full flex items-start p-4">
-                    <h1>Video will go here</h1>
+            <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className="relative">
+                {/* Video Layout */}
+                <div className="h-full flex items-start p-4 absolute inset-0">
+                    {layout === "grid" ? <PaginatedGridLayout /> : <SpeakerLayout />}
+
+                    {/* Participants List Overlay */}
+                    {showParticipants && (
+                      <div className="absolute right-0 top-0 h-full w-[300px] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+                        <CallParticipantsList onClose={() => setShowParticipants(false)} />
+                      </div>
+                    )}
+                        
                 </div>
             </ResizablePanel>
             
